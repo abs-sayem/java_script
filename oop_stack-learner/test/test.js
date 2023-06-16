6a659d096e6f39300aa80910fca94eeed8b0d7d5
ABS-Sayem <abssayem121194@gmail.com>
2023-06-16 13:56:48 +0600

    added test directory

---
+++ oop_stack-learner/test/test.js
@@ -0,0 +1,14 @@
+class MyInfo{
+    name = "Md Abu Bakar Siddiq Sayem"
+    email = "mdabssayem@gmail.com"
+    phone = "01825507837"
+
+    // constructor(){
+    //     this.name = name
+    //     this.email = email
+    //     this.phone = phone
+    // }
+}
+
+const myinfo1 = new MyInfo()
+console.log(myinfo1)
